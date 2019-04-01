443b0bb698bb19f58bcbed2a783b2df1091238e3
PhilipMalan1 <45105783+PhilipMalan1@users.noreply.github.com>
2019-04-01 13:47:21 -0500

    Added level class

---
+++ Blocks/Blocks/Level.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blocks
+{
+    class Level
+    {
+        private List<List<List<GameObject>>> levelObjects;
+        private int rowNum;
+
+        public Level(List<List<List<GameObject>>> levelObjects, int rowNum)
+        {
+            this.levelObjects = levelObjects;
+            this.rowNum = rowNum;
+        }
+
+        public int RowNum
+        {
+            get
+            {
+                return rowNum;
+            }
+        }
+
+        internal List<List<List<GameObject>>> LevelObjects
+        {
+            get
+            {
+                return levelObjects;
+            }
+
+            set
+            {
+                levelObjects = value;
+            }
+        }
+
+        public void AddGameObject(GameObject gameObject, int x, int y)
+        {
+            //add columns
+            while (x >= levelObjects.Count())
+                levelObjects.Add(new List<List<GameObject>>());
+            //add rows
+            while (y >= levelObjects.Count()) levelObjects[x].Add(new List<GameObject>());
+            //add object
+            levelObjects[x][y].Add(gameObject);
+        }
+
+        public void RemoveGameObject(int x, int y)
+        {
+            //if object exists
+            if (x<levelObjects.Count() && y<levelObjects[x].Count() && LevelObjects[x][y].Count>0)
+            {
+                //remove object
+                levelObjects[x][y].RemoveAt(levelObjects[x][y].Count() - 1);
+                //remove rows
+                while (levelObjects[x][levelObjects[x].Count() - 1].Count() <= 0)
+                    levelObjects[x].Remove(levelObjects[x][levelObjects[x].Count() - 1]);
+                //remove columns
+                while (levelObjects[levelObjects.Count() - 1].Count() <= 0)
+                    levelObjects.Remove(levelObjects[levelObjects.Count() - 1]);
+            }
+        }
+    }
+}
--- Blocks/Blocks/LevelEditorScreen.cs
+++ Blocks/Blocks/LevelEditorScreen.cs
@@ -9,20 +9,32 @@ namespace Blocks
 {
     class LevelEditorScreen : Screen
     {
-        private List<List<LinkedList<GameObject>>> level;
+        private Level level;
             
         public LevelEditorScreen(GraphicsDevice graphicsDevice, Game1 game1) : base(graphicsDevice, game1)
         {
+            List<List<List<GameObject>>> levelGrid = new List<List<List<GameObject>>>();
+            level = new Level(levelGrid, 0);
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            throw new NotImplementedException();
+            //draw level
+            foreach (List<List<GameObject>> column in level.LevelObjects)
+            {
+                foreach(List<GameObject> tile in column)
+                {
+                    foreach(GameObject gameObject in tile)
+                    {
+                        gameObject.Draw(gameTime);
+                    }
+                }
+            }
         }
 
         public override void Update(GameTime gameTime)
         {
-            throw new NotImplementedException();
+            
         }
     }
 }
