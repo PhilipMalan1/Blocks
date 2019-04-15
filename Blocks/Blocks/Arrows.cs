ec5cdf4084407054e5667442d2e060f0d9009321
CatLoverForLife <garrysnail4@gmail.com>
2019-04-15 14:13:15 -0500

    sa

--- Blocks/Blocks/Arrows.cs
+++ Blocks/Blocks/Arrows.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace Blocks
 {
-    class Arrows
+    class Arrows : GameObject
     {
+        // U = Up
+        // R = Right
+
         /*
              /\     /\
             /  \___/  \
@@ -16,7 +21,56 @@ namespace Blocks
            |           |
 
          */
+         
+        Texture2D arrowTex;
+        Rectangle arrowRec;
 
+        public Arrows(Level level, float blockWidth, Vector2 spawnPos) : base(level, blockWidth, spawnPos)
+        {
+        }
 
+        public override Vector2 Pos
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+
+        public override void NextDataValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void PreviousDataValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DataValueName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Initialize(Level level, float blockWidth)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spritebach, Vector2 camera)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
