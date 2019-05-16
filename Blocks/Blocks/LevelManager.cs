964ab045306de7ec726650bfffc5158bc45c36a3
PhilipMalan1 <45105783+PhilipMalan1@users.noreply.github.com>
2019-05-16 13:50:00 -0500

    Added levels 3 and 4 to the game

--- Blocks/Blocks/LevelManager.cs
+++ Blocks/Blocks/LevelManager.cs
@@ -7,7 +7,7 @@ namespace Blocks
 {
     class LevelManager
     {
-        private static string[] levels = { @"Content/Levels/Level 0.dat", @"Content/Levels/Level 1.dat", @"Content/Levels/philipLevel 2.dat" };
+        private static string[] levels = { @"Content/Levels/Level 0.dat", @"Content/Levels/Level 1.dat", @"Content/Levels/philipLevel 2.dat", @"Content/Levels/philipLevel 3.dat", @"Content/Levels/philipLevel 4.dat" };
 
         public static string firstLevel()
         {
